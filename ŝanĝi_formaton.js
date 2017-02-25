85ad59411a88e27c22c2a6905f4601a573db8c16
axolotl <andre.charles.lewin@gmail.com>
2017-02-25 21:30:59 +0100

    ebligis ŝanĝi la formaton de la temperaturo

--- ŝanĝi_formaton.js
+++ ŝanĝi_formaton.js
@@ -1,21 +1,27 @@
 $("#ŝanĝi_formaton").on("click", function () {
     var varmecoOutput = document.getElementById("varmeco");
-    var varmecoTeksto = varmecoOutput.innerHTML;
+    var varmecoTeksto = varmecoOutput.textContent;
 
     // Akiri la nombron
-
+    var regexNombro = /[+-]?\d+(\.\d+)?/;
+    var nombro = varmecoTeksto.match(regexNombro)[0];
 
     // Akiri la formaton
-
-
+    var regexFormato = /.$/;
+    var formato = varmecoTeksto.match(regexFormato)[0];
 
     // Kalkulo depende de la formato
+    var novaEnhavo = '404';
+    if (formato == 'F') {
+        novaEnhavo = (nombro - 32)*(5/9);
+        novaEnhavo += '°C';
+    } else if (formato == 'C') {
+        novaEnhavo = (nombro * 9/5) + 32;
+        novaEnhavo += '°F';
+    }
 
-
-
-    // Krei la novan tekston
-    novaTeksto = "Varmeco: Laboro farota";
+    novaEnhavo = 'Varmeco: ' + novaEnhavo;
 
     // Ŝanĝi la tekston
-    varmecoOutput.innerHTML = novaTeksto;
+    varmecoOutput.innerHTML = novaEnhavo;
 });
